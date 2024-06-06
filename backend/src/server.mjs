b1d3f44834054e88edcb7c0a29c687a5a117e290
serj2626 <serj2626@mail.ru>
2024-06-06 17:02:36 +0300

    added mongo client

--- backend/src/server.mjs
+++ backend/src/server.mjs
@@ -1,89 +1,23 @@
 import bodyParser from "body-parser";
 import express from "express";
+import { MongoClient } from "mongodb";
+
 const PORT = 3000;
 
-const products = [
-    {
-        id: 1,
-        name: "Кроссовки Nike Venture Runner",
-        material: "Синтетическая дышащая сетка",
-        price: "8800",
-        description: "shoes description",
-        image:"shoes2.jpg",
-        rating: "5"
-    },
-    {
-        id: 2,
-        name: "Кроссовки Niker",
-        material: "Синтетическая дышащая сетка",
-        price: "10500",
-        description: "shoes description",
-        image:"shoes3.jpg",
-        rating: "5"
-    },
-    {
-        id: 3,
-        name: "Кроссовки adidas Sportswear STRUTTER",
-        material: "Синтетическая дышащая сетка",
-        price: "12600",
-        description: "shoes description",
-        image:"shoes4.jpg",
-        rating: "5"
-    },
-    {
-        id: 4,
-        name: "Кроссовки adidas Galaxy Star M",
-        material: "Синтетическая дышащая сетка",
-        price: "11550",
-        description: "shoes description",
-        image:"shoes5.jpg",
-        rating: "5"
-    },
-    {
-        id: 5,
-        name: "Кроссовки BAASPLOA",
-        material: "Синтетическая дышащая сетка",
-        price: "7700",
-        description: "shoes description",
-        image:"shoes7.jpg",
-        rating: "5"
-    },
-    {
-        id: 6,
-        name: "Кроссовки PUMA Suede",
-        material: "Синтетическая дышащая сетка",
-        price: "3300",
-        description: "shoes description",
-        image:"shoes8.jpg",
-        rating: "5"
-    },
-    {
-        id: 7,
-        name: "Кроссовки Lion and Sons",
-        material: "Синтетическая дышащая сетка",
-        price: "5890",
-        description: "shoes description",
-        image:"shoes2.jpg",
-        rating: "5"
-    },
-    {
-        id: 8,
-        name: "Кроссовки PULSE",
-        material: "Синтетическая дышащая сетка",
-        price: "4400",
-        description: "shoes description",
-        image:"shoes2.jpg",
-        rating: "5"
-    },
-]
 
 const app = express();
 app.use(express.json());
 
 
 
-app.get("/products", (req, res) => {
-    res.send({ "All products": products });
+app.get("/products", async (req, res) => {
+    const client = await MongoClient.connect("mongodb://localhost:27017",
+        { useUnifiedTopology: true, useNewUrlParser: true });
+    const db = client.db("sneakers_db");
+    const products = await db.collection("products").find({}).toArray();
+    res
+        .status(200)
+        .send(products);
 });
 
 app.get("/products/:id", (req, res) => {

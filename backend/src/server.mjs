4007c52e98ccf6a19669d6ae4f365c869111c26f
serj2626 <serj2626@mail.ru>
2024-06-06 17:13:08 +0300

    created urls for products collections

--- backend/src/server.mjs
+++ backend/src/server.mjs
@@ -18,14 +18,26 @@ app.get("/products", async (req, res) => {
     res
         .status(200)
         .send(products);
+    client.close();
 });
 
-app.get("/products/:id", (req, res) => {
+app.get("/products/:id", async (req, res) => {
     let { id } = req.params;
     id = +id;
-    const product = products.find(product => product.id === id);
-    res.send({ "product": product });
+    const client = await MongoClient.connect("mongodb://localhost:27017",
+        { useUnifiedTopology: true, useNewUrlParser: true });
+    const db = client.db("sneakers_db");
+
+    if (isNaN(id)) return res.status(400).send({ msg: "Bad Request" });
+    const product = await db.collection("products").findOne({ id: id });
+    if (product) {
+        res.send({ "product": product });
+    } else {
+        res.status(404).send({ msg: "Product Not Found" });
+    }
+    client.close();
 });
+
 
 app.get("/uses:id/cart", (req, res) => {
     res.send({ "message": "Hello World!" });
